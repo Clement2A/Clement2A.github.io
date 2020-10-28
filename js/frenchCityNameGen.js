43555c3a1cf6b74e37486660d5fb8365664af4c8
Rygasary <rygasary@gmail.com>
2020-10-28 18:56:59 +0100

    French City Name Generator v1
    First implementation of my french city name generator

---
+++ js/frenchCityNameGen.js
@@ -0,0 +1,119 @@
+var debut = ["Sa","Sab","Sar","Sam","San","Pa","Sau","Saul","Va","Vit","Val","Yvr","Ve","Vou","Vouil","Ville","Vi","Villen","Vil","Vé","Ves","Vern","Ver","Ven","Vau","Vel","Valen","Varen","Valle","Valli","Tré","Tu","Tre","Troi","Trou","Tos","Tourne","Tou","Tour","Ti","Thié","To","Tor","Thi","Tho","Thén","Thé","Tar","Tart","Tau","Ta","Te","Ter","Taille","Tal","Sey","Sausse","Su","Sur","Sou","Sous","So","Som","Son","Sig","Si","Sir","Soi","Sér","Ser","Sem","Sen","Se","Sei","Seil","Sé","Savi","Saut","Sauv","Sar","Pai","Pal","Pala","Ra","Rai","Ran","Per","Ru","Rum","Rei","Ros","Roy","Rous","Rouv","Rouss","Roue","Rou","Rot","Ron","Roi","Ro","Rob","Roc","Rein","Rev","Riv","Remi","Ren","Renne","Ri","Ri","Rim","Rieu","Rem","Pon","Re","Ré","Rea","Rec","Pons","Pont","Prad","Qui","Quin","Pro","Quan","Que","Pu","Puy","Pus","Pru","Prun","Pre","Pres","Preu","Pra","Pou","Poui","Por","Pers","Pan","Pey","Pla","Plou","Poi","Pois","Pol","Poli","Plai","Plei","Plein","Pleu","Plain","Peyr","Pierre","Pi","Pier","Pis","Pi","Panne","Paul","Per","Perri","Pes","Pet","Pel","Pen","Penne","Pé","Bas","Pas","Par","Na","Ou","Oi","Ol","Orv","Os","Or","Ormo","Org","Nan","Ni","Nu","Nui","Noy","No","Nou","Nov","Noi","Non","Mi","Nau","Nav","Ne","Neuf","Neu", "Neuil","Neuv","Mour","Mouli","Mu","Mur","Mout","Moy","Mou","Mors","Mor","Montva","Mond","Montro","Mon","Monto","Monti","Montga","Monten","Monté","Mira","Mont","Monta","Mons","Mir","Mis","Mol","Moli","Marci","Mes","Meu","Mil","Mey","Méz","Méri","Mai","Men","Méne","Méni","Marn","Maur","Maure","Mei","Meil","Meli","Melle","Me","Mau","Maz","Mazi","Mas","Mart","Margen","Mari","Man","Mar","Maran","Marc","Marco","Malau","Mal","Gui","Ma","Mach","Par","Lugny","Lus","Ly","Jon","Li","Lu","Lor","Lou","Lour","Loup","Loua","Long","Loc","Lign","Ligni","Laver","Lez","Les","Lespa","Lespi","Lem","Laca","Land","Lam","Las","La","Le","Lan","Lag","Labruy","Laboi","Labou","La","Jou","Juvi","Jour","Jus","Jal","Jan","Herb","Ho","Is","Ille","Illi","Humb","Hom","Hénan","Heud","Hau","Guyen","Ham","Han","Hal","Hallo","Guizan","Guer","Guitran","Gris","Gros","Gué","Guer","Gri","Grim","Cor","Glan","Gil","Grav","Grain","Gra","Gram","Grand","Gour","Gou","Gomme","Gon","Ges","Gi","Givr","Glan","Abon","Donne","Dor","Douau","Écha","Germ","Ge","Ger","Gel","Gen","Gas","Fal","Gau","Ga","Garre","Gau","Fourque","Hé","Gar","Gail","Gal","Fu","Fonte","Fo","Four","Forges","Form","Fosse","Fonta","Font","Folle","Fleury","Foi","Fon","Bourgui","Franc","Freis","Frièr","Fla","Flamme","Flavi","Flassi","Froid","Friche","Fré","Fra","Franche","Flo","Fig","Flaman","Floir","Fla","Flas","Flan","Fil","Fest","Feuche","Feuil","Feu","Fenou","Ev","Écail","Don","Vi","Dur","Écu","Esp","Est","Éta","Écha","Éque","Éti","Équan","Esco","Esca","Équi","Écail","Échi","Entre","Entrai","En","Espa","Espé","Éver","Évi","Espin","Er","Ala","Argil","Ar","Vir","Lil","Auri","Bardon","Baf","Cler","Blanche","Sei","Bain","Co","Bo","Bois","Bon","Boigne","Bor","Bouque","Béalan","Beau","Bu","Caba","Casa","Cadei","Cadil","Castel","Co","Bre","Chaba","Chaille","Chantil","Chain","Tro","Cal","Cras","Crant","Cra","Crain","Sei","Mar","Champ","Neuil","Dai","Dam","Dagon","Dom"];
+var fin = ["pont","net","ge","elle","ière","fort","ières","bosc","avant","cienne","oux","loup","bourg","illon","cieu","quère","illy","roche","roque","tour","range","drecourt","vigny","mans","tia","ette","sans","is","vi","te","ta","igny","lières","ly","re","nat","on","vres","ons","rolles","nin","bertin","manant","aine","chères","magne","montiers","arol","poir","gues","ères","aise","taine","lon","chon","reux","illet","gnon","villers","bonne","nay","eyrac","treux","lette","villy","vernes","fort","rolles","main","chaux","ault","court","tol","le","ac","mont","ne","ville","gnac","deau","val","champs","chastel","che","chy","chêne","court","gniers","tagne","vois","treaux","gnon","denac","roi","gny","ne","seil","agne","marie","mard"];
+var milieu = ["-et-","-sur-","-en-","-Saint-","-le-","-la-","-Sainte-","-sous-","-les-","-en-","-le-Grand-","-la-Grande-","-au-"];
+var compose = ["Lazare","Dordogne","Lamartine","Roc","Aix","Rive","Rhône","Mary","Belfore","Montagne","Moselle","Garonne","Roche","Mer","Forêt","Beaumont","Eau","Rivière","Fleury","Villette","Loire","Chapelle","Compte","Seine","Mérogis","Beaurain","Val","Sarthe","Meuse","Anjou","Chalet","Franc","Duc","Lys","Tech","Pompadour","Poste","Companie","Ferrand","Bain","Église","Bain","Marne","Château","Prignac","Orne","Martin","Louis","Maclou","Lorraine","Adieu","Bois","Marais","Champagne","Rocquefort","René","Denis","Prairie"];
+
+function beginGen(){
+    if(window.document.getElementById("resetOnGen").checked){
+        resetHTML("cityList");
+    }
+        genFrCityName(window.document.getElementById("nbGen").value);
+}
+
+function genFrCityName(nbCity){
+    for(var i = 0; i < nbCity; i++){
+        var nomVille = "";
+        var debutChoisi = debut[Math.floor(Math.random() * debut.length)];
+        var finChoisi = fin[Math.floor(Math.random() * fin.length)];
+        
+        if(debutChoisi.slice(-1) == "i" && finChoisi.slice(0,1) == "i"){
+           debutChoisi = debutChoisi.slice(0,-1);
+        }
+        
+        if(debutChoisi.slice(-1) == "o" && finChoisi.slice(0,1) == "o"){
+           debutChoisi = debutChoisi.slice(0,-1);
+        }
+        
+        if(debutChoisi.slice(-1) == "a" && finChoisi.slice(0,1) == "a"){
+           debutChoisi = debutChoisi.slice(0,-1);
+        }
+        
+        if(debutChoisi.slice(-1) == "t" && finChoisi.slice(0,1) == "v"){
+           if(randomInt(1,2) == 2){
+               debutChoisi = debutChoisi.slice(0,-1)
+           }else{
+               finChoisi = finChoisi.slice(1);
+           }
+        }
+        
+        if(debutChoisi.slice(-1) == "d" && finChoisi.slice(0,1) == "t"){
+           if(randomInt(1,2) == 1){
+               debutChoisi = debutChoisi.slice(0,-1)
+           }else{
+               finChoisi = finChoisi.slice(1);
+           }
+        }
+        
+        if(debutChoisi.slice(-1) == "d" && finChoisi.slice(0,2) == "ch"){
+           if(randomInt(1,2) == 1){
+               debutChoisi = debutChoisi.slice(0,-1)
+           }else{
+               finChoisi = finChoisi.slice(2);
+           }
+        }
+        
+        if(debutChoisi.slice(0,1) == "H" || debutChoisi.slice(0,1) == "A" || debutChoisi.slice(0,1) == "E" || debutChoisi.slice(0,1) == "I" || debutChoisi.slice(0,1) == "O" || debutChoisi.slice(0,1) == "U" || debutChoisi.slice(0,1) == "Y" || debutChoisi.slice(0,1) == "É"){
+            if(randomInt(1,20) == 1){
+                nomVille = "L'";
+            }
+        }else{
+            if(randomInt(1,20) == 1){
+                if(randomInt(1,2) == 1){
+                    nomVille = "Le ";
+                }else{
+                    nomVille = "La ";
+                }
+            }
+        }
+        
+        if(randomInt(1,10) == 1){
+            if(randomInt(1,20) == 1){
+                if(randomInt(1,15) == 1){
+                    nomVille = "L'";
+                }
+                if(randomInt(1,2) == 1){
+                    nomVille += "Haut-";
+                }else{
+                    nomVille += "Haute-";
+                }
+            }else{
+                if(randomInt(1,2) == 1){
+                    nomVille = "Saint-";
+                }else{
+                    nomVille = "Sainte-";
+                }
+            }
+        }
+        
+        nomVille += debutChoisi;
+        nomVille += finChoisi;
+        
+        if(randomInt(1,10) == 1){
+            var deter = milieu[Math.floor(Math.random() * milieu.length)];
+            nomVille += deter;
+            nomVille += compose[Math.floor(Math.random() * compose.length)];
+            if(deter == "-les-"){
+                if(nomVille.slice(-1) != "s" && nomVille.slice(-1) != "x"){
+                    if(nomVille.slice(-1) != "u"){
+                        nomVille += "s";
+                    }else{
+                        nomVille += "x";
+                    }
+                }
+            }
+        }else if(randomInt(1,9) == 1){
+            nomVille += "-";
+            nomVille += compose[Math.floor(Math.random() * compose.length)];
+        }
+        
+        nomVille = nomVille.toUpperCase();
+        var outerPanel = document.createElement("div");
+        outerPanel.className = "outerCityPanel";
+        var panel = document.createElement("div");
+        panel.className = "cityPanel";
+        var cityName = document.createTextNode(nomVille);
+        panel.appendChild(cityName);
+        outerPanel.appendChild(panel);
+        window.document.getElementById("cityList").appendChild(outerPanel);
+    }
+}
